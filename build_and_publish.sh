6dc9b8ca6f1ae9e0c6f798b4ad6f041994744509
Robin Harms Oredsson <robin@betahaus.net>
2023-03-08 15:42:59 +0100

    Cleanup

--- build_and_publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Only builds a generic nginx container. "
-echo "Create a volume targeted at /app/public_html"
-docker build . -t voteit/static_nginx:latest
-docker push voteit/static_nginx:latest
